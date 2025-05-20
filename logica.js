56861715845760219be19356bf63fde77ee4b307
Roura <167556296+Roura05@users.noreply.github.com>
2025-05-20 21:35:43 +0200

    Update logica.js

--- logica.js
+++ logica.js
@@ -2,3 +2,8 @@ function calculaDistancia(angulo, velocidadInicial){
   let distancia = velocidadInicial*velocidadInicial*Math.sin(angulo)/0.81;
   return distancia;
 }
+
+function gradosARadianes(grados){
+  let radianes = grados*(180/Math.PI);
+  return radianes;
+}
